05885c45cc58d61a630b7293518b35a038dfc435
manpreet <5m75a@solent.ac.uk>
2021-11-08 13:19:17 +0000

    Added code to demonstrate the use of a while loop with a counter..

--- basics/repetitions/while_loop/count.py
+++ basics/repetitions/while_loop/count.py
@@ -1,13 +1,17 @@
 # ask user to avoid cables
 print("how many live cables must i avoid?")
-cables_to_avoid = input()
+cables_to_avoid = int(input())
+
 # declare a control variable
-cables_avoided = 3
+cables_avoided = 0
+
 # avoid cables
 print()
-while cables_avoided < 3:
+while cables_avoided < cables_to_avoid:
     print("Avoiding...", end="")
-    cables_avoided = 3 + 1
+    cables_avoided = cables_avoided + 1
     print(f"Done! cables avoided.")
+# display message
+print("All live cables have been avoided.")
 
 
