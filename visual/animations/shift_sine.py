d04dfe09925db5f0cc5628b0cc931c478396aa1e
manpreet <5m75a@solent.ac.uk>
2021-11-15 14:51:00 +0000

    Added code to visualise a shifting sine wave..

---
+++ visual/animations/shift_sine.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
+
+fig, ax = plt.subplots()
+
+
+def animate(frame):
+    global ax
+    ax.cla()
+    ax.set_xlim(0, 720)
+    ax.set_ylim(-1, 1)
+    x = range(0, 360)
+    y = [math.sin(math.radians(degrees) + (frame / 50)) for degrees in x]
+
+    ax.plot(x, y)
+
+
+def run():
+    global line
+    sine_animation = animation.FuncAnimation(fig,
+                                             animate,
+                                             frames=720,
+                                             interval=100)
+    plt.show()
+
+
+run()
