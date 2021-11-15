5e6c7908c1bc96e630582ae7f5831916cf205b6e
manpreet <5m75a@solent.ac.uk>
2021-11-15 14:47:12 +0000

    Added code to visualise a sine wave..

---
+++ visual/animations/sine.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import math
+
+fig, ax = plt.subplots()
+
+def animate(frame):
+  global ax
+  ax.set_xlim(0, 720)
+  ax.set_ylim(-1, 1)
+  x = range(0, frame)
+
+  y = []
+  for degrees in x:
+    y.append(math.sin(math.radians(degrees)))
+
+  # Alternatively, we can use a list comprehension as shown below:
+  # y = [math.sin(math.radians(degrees)) for degrees in x]
+
+  ax.plot(x, y)
+
+def run():
+  global line
+  sine_animation = animation.FuncAnimation( fig,
+                                            animate,
+                                            frames = 720,
+                                            interval = 100)
+  plt.show()
+
+run()
