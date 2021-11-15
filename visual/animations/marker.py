2deed2dd8ced44fe8718b4acb2a3ef6e223493a4
manpreet <5m75a@solent.ac.uk>
2021-11-15 14:44:45 +0000

    Added code to display an animated marker.

---
+++ visual/animations/marker.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+line = None
+
+def animate(frame):
+  global line
+  line.set_data(frame, frame)
+
+def run():
+  global line
+  fig, ax = plt.subplots()
+  ax.set_xlim(0, 10)
+  ax.set_ylim(0, 10)
+  line, = ax.plot([], [], 'ro')
+  simple_animation = animation.FuncAnimation( fig, animate, frames = 10, interval = 1000)
+  plt.show()
+
+run()
