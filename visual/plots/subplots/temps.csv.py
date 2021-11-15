5ffb561be3fff749baa781be72f8b630d3326ada
manpreet <5m75a@solent.ac.uk>
2021-11-15 13:20:02 +0000

    Added code to display simple subplots.

---
+++ visual/plots/subplots/temps.csv.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import csv
+
+
+def read_data():
+    with open('visual/subplots/temps.csv') as file:
+        csv_reader = csv.reader(file)
+
+        header = next(csv_reader)
+        data = {'week1': [], 'week2': []}
+
+        for line in csv_reader:
+            data['week1'].append(float(line[0].strip()))
+            data['week2'].append(float(line[1].strip()))
+
+    return data
+
+
+def run():
+    data = read_data()
+
+    fig, (ax1, ax2) = plt.subplots(1, 2)
+    ax1.plot(range(len(data['week1'])), data['week1'])
+    ax2.plot(range(len(data['week2'])), data['week2'])
+    plt.show()
+
+
+run()
