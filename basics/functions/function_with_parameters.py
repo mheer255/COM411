67ae6a5b3b4ae23143ca68e1e55ba7f7f416ced7
manpreet <5m75a@solent.ac.uk>
2021-11-18 23:06:13 +0000

    Added code to demonstrate the use of a user-defined function with multiple parameters..

--- basics/functions/function_with_parameters.py
+++ basics/functions/function_with_parameters.py
@@ -1,9 +1,11 @@
- ef climb_ladder("steps_remaining, steps_crossed"):
+def climb_ladder(steps_remaining, steps_crossed):
     # Compare and display a suitable message
     if steps_remaining > steps_crossed:
         print("Still some way to go!")
     else:
+
         print("We are almost there!")
+
 
 # calls to function
 climb_ladder(5, 2)
